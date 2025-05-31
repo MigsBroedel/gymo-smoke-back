5e2e22b33c05db8f810baa780c3c58eb3c0de14d
MigsBroed <miguel.broedel09@gmail.com>
2025-05-31 10:08:44 -0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://gymo-smoke.vercel.app', 
+    origin: ['https://gymo-smoke.vercel.app', 'https://gymo-ten.vercel.app'], 
     credentials: true, 
   });
 
